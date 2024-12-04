e7960f138108344d37bb97aac23bb90ff71bb5d1
GabrielFogo <gabrielcostal164@gmail.com>
2024-12-04 20:37:31 -0300

    feat: day 1 part 2

---
+++ day1part2/Program.cs
@@ -0,0 +1,31 @@
+﻿var relativePath = @"..\..\..\..\input.txt";
+var absolutePath = Path.GetFullPath(relativePath);
+
+List<int> leftColumn = [];
+List<int> rightCollum = [];
+List<int> similarityScore = [];
+
+try
+{
+    var lines = File.ReadLines(absolutePath);
+
+    foreach (var line in lines)
+    {
+        var colluns = line.Split("   ");
+        leftColumn.Add(int.Parse(colluns[0].Trim()));
+        rightCollum.Add(int.Parse(colluns[1].Trim()));
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+    throw;
+}
+
+foreach (var number in leftColumn)
+{
+    var appearsInTheSecondCollum = rightCollum.Count(sc => sc == number);
+    similarityScore.Add(number * appearsInTheSecondCollum);
+}
+
+Console.WriteLine(similarityScore.Sum());
