ee34dc0ee6be3614d80a73d1376120655d8b772e
GabrielFogo <gabrielcostal164@gmail.com>
2024-12-04 20:34:36 -0300

    feat: day 1 part 1

---
+++ day1part1/Program.cs
@@ -0,0 +1,34 @@
+﻿var relativePath = @"..\..\..\..\input.txt";
+var absolutePath = Path.GetFullPath(relativePath);
+
+List<int> leftColumn = [];
+List<int> rightColmn= [];
+List<int> distances = [];
+
+try
+{
+    var lines = File.ReadLines(absolutePath);
+
+    foreach (var line in lines)
+    {
+        var colluns = line.Split("   ");
+        leftColumn.Add(int.Parse(colluns[0].Trim()));
+        rightColmn.Add(int.Parse(colluns[1].Trim()));
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+    throw;
+}
+
+var firstCollumOrded = leftColumn.Order().ToList();
+var secondCollumOrded = rightColmn.Order().ToList();
+
+for (int i = 0; i < firstCollumOrded.Count; i++)
+{
+    var distance = secondCollumOrded[i] - firstCollumOrded[i];
+    distances.Add(Math.Abs(distance));
+}
+
+Console.WriteLine(distances.Sum());
